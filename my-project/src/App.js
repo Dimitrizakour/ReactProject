88b700052b02221b30e03d17abdbf137c81cd8e2
Dimitrizakour <dimitrizakkour@gmail.com>
2022-07-15 14:49:54 +0300

    change paragraph

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const[blur,setblur]= useState('blur');
+
+  
+  const toogleHandler =()=>{
+
+      if(blur==='blur')
+      {
+        setblur('blur-none');
+      }
+      else{
+        setblur('blur');
+      }
+
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         This is a change 2.
+        <p className={'text-4xl mb-10 '+blur} >
+         Surpirse ;)
         </p>
+        <button className='bg-blue-500 px-5 rounded-3xl mb-10' onClick={toogleHandler}>CLICK ME</button>
         <a
           className="App-link"
           href="https://reactjs.org"
