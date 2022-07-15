7113613e51f92a84f8fb947b9ade919d258df596
Dimitrizakour <dimitrizakkour@gmail.com>
2022-07-15 12:55:47 +0300

    change paragraph

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is a change.
+         This is a change 2.
         </p>
         <a
           className="App-link"
